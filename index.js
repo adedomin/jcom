cc452b46e104337ce0b484ad30f3a8da8cc5eb85
prussian <genunrest@gmail.com>
2016-11-27 13:33:11 -0500

    interactive shell element added

---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016  prussian <genunrest@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var SHELL_VARS = {}
+
+var readline = require('readline'),
+    parser = require('./parse/line').parse,
+    pipeline = require('./lib/pipeline'),
+    builtins = require('./lib/core-builtins')
+
+var templ = (line) => `(function() {
+    with(this)
+    return \`${line}\`
+}).bind(SHELL_VARS)()`
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+var linebuffer = ''
+rl.on('line', line => {
+    if (line.slice(-1) == '\\') {
+        linebuffer += line.slice(0, -2)
+        return
+    }
+    linebuffer += line
+    linebuffer = eval(templ(linebuffer))
+    
+    try {
+        linebuffer = parser(linebuffer)
+    }
+    catch (e) {
+        console.log(e)
+        linebuffer = ''
+        return
+    }
+
+    var pipe = pipeline(linebuffer, null, builtins)
+    if (pipe) pipe.pipe(process.stdout)
+    linebuffer = ''
+})
